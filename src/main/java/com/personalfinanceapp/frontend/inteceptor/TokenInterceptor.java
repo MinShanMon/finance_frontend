b65428e105bfd99bb61d746a04921d706d44f97b
Ivan <iv4n.eng.2@gmail.com>
2023-02-09 17:09:59 +0800

    removed token interceptor

--- src/main/java/com/personalfinanceapp/frontend/inteceptor/TokenInterceptor.java
+++
@@ -1,9 +0,0 @@
-// package com.personalfinanceapp.frontend.inteceptor;
-
-// import org.springframework.stereotype.Component;
-
-
-// @Component
-// public class TokenInterceptor implements HandlerInterceptor {
-    
-// }
